acf64efafa6f38bfa9f1035c41b06f7571e197d6
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2022-03-19 22:45:50 -0700

    added misc helper funcs IBO, normalize, pad border

--- helper_funcs.py
+++ helper_funcs.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Size of Thermal Camera Arrays
 T_m = 4
@@ -50,11 +51,36 @@ def IBO(arr: np.array):
     arr = normalize(arr)
     return arr
 
-# def generate_QTable(thermal, distance, t_range, d_range):
-#     qtable = np.zeros(((t_range[1] - t_range[0])*T_m*T_n*(d_range[1] - d_range[0])*4, 5))
-#     for t_val in range(t_range[0], t_range[1]):
-#         for d_val in range(d_range[0], d_range[1]):
-#             for t_x_idx in range(T_n):
-#                 for t_y_idx in range(T_m):
-#                     for d_idx in range(3):
-#                         qtable[0, :] = (np.array([]))
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+def plot_image(i, predictions_array, true_label, img):
+  true_label, img = true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  true_label = true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
