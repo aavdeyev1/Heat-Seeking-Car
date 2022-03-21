78783b9ce81856b689863e24b943aab9d86caa7b
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2022-03-20 21:55:29 -0700

    changed to all-string data

--- mqtt_example.py
+++ mqtt_example.py
@@ -1,13 +1,16 @@
-"""Script to generate next turn for heat-seeking autonomous car."""
-
-from ast import Pass
+"""MQTT Tx/Rx tranmission example"""
 import paho.mqtt.client as mqtt
+import time
 
 import paho.mqtt.publish as publish
 
 import connection
 
-DATA = connection.Buffer()
+dist_idx = (0, 4)
+gps_idx = (772, 773)
+mock_t = [float(i) for i in range(775)]
+mock_arr = str(mock_t)
+DATA_BUFFER = connection.Buffer(dist_idx, gps_idx)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -17,53 +20,69 @@ def on_connect(client, userdata, flags, rc):
     # reconnect then subscriptions will be renewed.
     client.subscribe("HeatSeekingCar/rx_thermal")
     client.subscribe("HeatSeekingCar/rx_distances")
+    client.subscribe("HeatSeekingCar/rx")
  
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
     if msg.topic == "HeatSeekingCar/rx_thermal":
-        DATA.thermal = msg.payload
+        DATA_BUFFER.thermal = msg.payload
 
     elif msg.topic == "HeatSeekingCar/rx_distances":
-        DATA.distance = msg.payload
+        DATA_BUFFER.distance = msg.payload
 
+    elif msg.topic == "HeatSeekingCar/rx":
+        DATA_BUFFER.msg = msg.payload
+        DATA_BUFFER.msg_flag = True
 
-mock_distance = [i for i in range(4)]
-mock_lat = 11.99999999999
-mock_long = 10.99999999999
-path = r"/Users/amelyaavdeyev/capstone_data/877.jpg"
+# mock_lat = 11.99999999999
+# mock_long = 10.99999999999
+# path = r"/Users/amelyaavdeyev/capstone_data/877.jpg"
 
-f=open(path, "rb")
-fileContent = f.read()
-byteArr = bytearray(fileContent)
+# f=open(path, "rb")
+# fileContent = f.read()
+# byteArr = bytearray(fileContent)
 
-mock_msg = [0, 1, 2, 3, mock_lat, mock_long]
-print(f"SENT: {mock_msg}")
+# mock_msg = [0, 1, 2, 3, mock_lat, mock_long]
+# print(f"SENT: {mock_msg}")
 
-# publish.single("HeatSeekingCar/test", byteArr, hostname="test.mosquitto.org")
-# publish.single("HeatSeekingCar/test", f"{mock_msg}", hostname="test.mosquitto.org")
+# publish.single("HeatSeekingCar/tx", byteArr, hostname="test.mosquitto.org")
+
+# publish.single("HeatSeekingCar/tx", f"{mock_arr}", hostname="test.mosquitto.org")
 # print("Done")
-
-# Recieve Message
-car = mqtt.Client()
 
 # Create an MQTT client and attach our routines to it.
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
- 
 client.connect("test.mosquitto.org", 1883, 60)
 
 client.loop_start()
 
-import time
+# Timeout after 15 mins
 t_end = time.time() + 60 * 15
 while time.time() < t_end:
-    time.sleep(1)
-    print(DATA)
+    time.sleep(2.5)
+    print(DATA_BUFFER.msg_flag)
+    if DATA_BUFFER.msg_flag: # If buffer not empty, send it to data pipeline
+        print(f"Message Recieved\nConverting Buffer to Arrays...")
+        # Translate from raw data to buffer
+        tick = time.time()
+        t_array, d_array, lat, long = DATA_BUFFER.buffer_to_arrays()
+        tock = time.time()
+        print(f">Process Time: {tock - tick}\n> {d_array}\n({lat}, {long})")
 
+        # Pass Info to Data Pipeline
 
-# Create Pipeline object, feed message from client
+        # Recieve Next Turn
+        mock_turn = -1
+
+        # Transmit Next Turn
+        print("Transmitting Next Turn to RC car...")
+        publish.single("HeatSeekingCar/tx", f"{mock_turn}", hostname="test.mosquitto.org")
+        DATA_BUFFER.msg_flag = False
+
+# A second stop loop will be if the target reached, or num_turns is at max
 client.loop_stop()
