a8a63e7fe12d933026768e9b6cf76510d4eef848
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2022-05-09 11:32:27 -0700

    rename connection to buffer file

--- buffer.py
+++ buffer.py
@@ -3,6 +3,7 @@
 /usr/local/sbin/mosquitto -c /usr/local/etc/mosquitto/mosquitto.conf
 """
 import numpy as np
+import tensorflow as tf
 
 t_dim = (24, 32)
 
@@ -24,22 +25,41 @@ class Buffer():
         for element in str(self.msg).replace("'", "").replace("b", ""). replace("[", "").replace("]", "").split(", "):
             temp.append(float(element))
 
+        # for element in str(self.msg).replace("'", "").replace("b", ""). replace("[", "").replace("]", "").replace("\n", "").split(" "):
+        #     temp.append(float(element))
+
         t_array = np.array(temp[self.dist_idx[1]:self.gps_idx[0]], dtype=np.float32).reshape(t_dim)
-        d_array = np.array(temp[ self.dist_idx[0]:self.dist_idx[1] ], dtype=np.int32)
+        d_array = np.array(temp[ self.dist_idx[0]:self.dist_idx[1] ], dtype=np.float32)
         lat = temp[self.gps_idx[0]]
         long = temp[self.gps_idx[1]]
 
         return t_array, d_array, lat, long
 
+
 if __name__ == "__main__":
     print("Running Check...")
     dist_idx = (0, 4)
     gps_idx = (772, 773)
-    mock_t = [float(i) for i in range(775)]
+    mock_t = [float(i) for i in range(774)]
     mock_arr = "b'"+str(mock_t)
     check = np.array(mock_t, dtype=np.float32)
     print(check)
 
     buf = Buffer(dist_idx, gps_idx)
     buf.msg = mock_arr
-    print(buf.buffer_to_arrays())
+    t_array, d_array, lat, long = buf.buffer_to_arrays()
+
+    x = tf.convert_to_tensor(
+                t_array, dtype=np.float32, dtype_hint=None, name=None)
+    x = tf.reshape(x, (1, 24, 32, 1))
+    avg_pool_2d = tf.keras.layers.AveragePooling2D(pool_size=(2, 2),
+            strides=(2, 2), padding='valid')
+    x = avg_pool_2d(x)
+    x = tf.reshape(x, (12, 16))
+    print(x)
+
+    d_array_test = [25, 44, 57, 99]
+    
+    for idx, element in enumerate(d_array_test):
+        d_array_test[idx] = round(element/10)*10
+    print(d_array_test)
