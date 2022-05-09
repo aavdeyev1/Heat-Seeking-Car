ba420891fdbf1dabf4293a02c3287707f5120325
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2022-05-09 11:33:39 -0700

    modify for variable input dims(downsampling prep)

--- environment.py
+++ environment.py
@@ -16,6 +16,7 @@ from __future__ import print_function
 import helper_funcs as hf
 from pipeline import Pipeline
 from pipeline_historian import PipelineHistorian
+from connector import Connector
 
 import tensorflow as tf
 import numpy as np
@@ -31,158 +32,169 @@ from tf_agents.environments import suite_gym
 from tf_agents.trajectories import time_step as ts
 
 TIMOUT_MINS = 5
-t_dim = (24, 32)
+# t_dim = (24, 32)
+t_dim = (12, 16)
 
 # reset_t_array = np.zeros((24,32), dtype=np.float32)
-reset_observation_vector = np.zeros((6,24,32), dtype=np.float32)
+reset_observation_vector = np.zeros((6,t_dim[0],t_dim[1]), dtype=np.float32)
 reset_observation_vector[:, 0, 0] = [500.0, 500.0, 500.0, 500.0, 0.1, 0]
 
 def mock_buffer_to_arrays(old_d, old_t):
-  new_d = old_d
-  new_t = old_t
-  new_t = new_t*new_t
-  return new_d, new_t
+    new_d = old_d
+    new_t = old_t
+    new_t = new_t*new_t
+    return new_d, new_t
 
 class RCCarEnv(py_environment.PyEnvironment):
 
-  def __init__(self):
-    self._action_spec = array_spec.BoundedArraySpec(
-        shape=(), dtype=np.int32, minimum=0, maximum=3, name='action')
-    
-    # [N, E, S, W, %] distances, current % of thermal view
-    self._observation_spec = array_spec.BoundedArraySpec(
-        shape=(6,24,32), dtype=np.float32, minimum=0, maximum=500.0, name='observation')
+    def __init__(self):
+        self._action_spec = array_spec.BoundedArraySpec(
+            shape=(), dtype=np.int32, minimum=0, maximum=3, name='action')
+        
+        # [N, E, S, W, %] distances, current % of thermal view
+        self._observation_spec = array_spec.BoundedArraySpec(
+            shape=(6,t_dim[0],t_dim[1]), dtype=np.float32, minimum=0, maximum=500.0, name='observation')
 
-    self._observation = reset_observation_vector
+        self._observation = reset_observation_vector
 
-    self._timout_after = time.time() + 60 * TIMOUT_MINS
+        self._timout_after = time.time() + 60 * TIMOUT_MINS
 
-    # current % of thermal view
-    self._state = self._observation[4, 0, 0]
-    self._episode_ended = False
-    self._pl_hist = PipelineHistorian()
+        # current % of thermal view
+        self._state = self._observation[4, 0, 0]
+        self._episode_ended = False
+        self._pl_hist = PipelineHistorian()
 
-  def action_spec(self):
-    return self._action_spec
+        self._observation_space_size = t_dim[0]*t_dim[1]*(self._action_spec.maximum + 1)
 
-  def observation_spec(self):
-    return self._observation_spec
+        self.conn = Connector()
 
-  def _reset(self):
-    self._episode_ended = False
-    self._pl_hist.reset()
-    self._observation = reset_observation_vector
-    return ts.restart(reset_observation_vector)
+    def action_spec(self):
+        return self._action_spec
 
-  def _step(self, next_action):
-    
-    if self._episode_ended:
-      # The last action ended the episode. Ignore the current action and start
-      # a new episode.
-      return self.reset()
-    
-    # Make sure episodes don't go on forever.
-    if self._observation[4, 0, 0] > .85:
-      # End episode if target found, or T% is > .85
-      print("Target Found, ending episode...")
-      # Send Stop command to car
-      self._episode_ended = True
-    elif time.time() > self._timout_after:
-      print("Timeout...")
-      # Send Stop command to car
-      self._episode_ended = True
-    elif next_action >= 0 and next_action < 4:
-      # Calculate prev/current reward
-      prev_pl = self._pl_hist.current_pl
-      prev_d_score = prev_pl.d_score
-      # prev_reward = prev_pl.get_reward(prev_t_percent=prev_t, prev_d_score=prev_d)
+    def observation_spec(self):
+        return self._observation_spec
 
-      # busy wait til next Buffer comes in
-      # TODO: remove + replace with while not DATA_BUFFER.msg_flag:
-      while False:
-        pass
+    def _reset(self):
+        self._episode_ended = False
+        self._pl_hist.reset()
+        self._observation = reset_observation_vector
+        return ts.restart(reset_observation_vector)
 
-      # Convert to usable arrays NUMPY arrs
-      # t_array, d_array, lat, long = DATA_BUFFER.buffer_to_arrays()
-      mock_t = np.array([float(i) for i in range(768)], dtype=np.float32).reshape(t_dim)
-      mock_d = np.array([100, 100, 25, 25], dtype=np.float32)
+    def _step(self, next_action):
+        # TODO: uncomment  when figure out attribute not found error..
+        # self.conn.check_timeout()
+        lat = None
 
-      # Generate pipeline for next step
-      new_pipeline = Pipeline(mock_t, mock_d, prev_t_percent=self._observation[4], prev_d_score=prev_d_score)
+        if self._episode_ended:
+            # The last action ended the episode. Ignore the current action and start
+            # a new episode.
+            return self.reset()
+        
+        # Make sure episodes don't go on forever.
+        if self._observation[4, 0, 0] > .85:
+            # End episode if target found, or T% is > .85
+            print("Target Found, ending episode...")
+            # Send Stop command to car
+            self._episode_ended = True
+        elif time.time() > self._timout_after:
+            print("Timeout...")
+            # Send Stop command to car
+            self._episode_ended = True
+        elif next_action >= 0 and next_action < 4:
+            # Calculate prev/current reward
+            prev_pl = self._pl_hist.current_pl
+            prev_d_score = prev_pl.d_score
+            # prev_reward = prev_pl.get_reward(prev_t_percent=prev_t, prev_d_score=prev_d)
 
-      # TODO - remove
-      new_pipeline.t_percent = self._observation[4, 0, 0]+.2
+            # busy wait til next Buffer comes in
+            while lat == None:
+                t_array, d_array, lat, long = self.conn.check_for_data()
+                time.sleepsleep(1)
+            
+        else:
+            raise ValueError('`action` should be 0 to 3.')
 
-      self._pl_hist.add_step(new_pipeline)
+        if self._episode_ended or self._observation[4, 0, 0] > .85:
+            # reward = self._pls[self._pl_idx].get_reward(self._observation_spec)
+            return ts.termination(reset_observation_vector, reward=100)
+        else:
+            # Valid action, carry out next turn
+            self.conn.send_turn(next_action)
+            # TODO: remove + replace with while not DATA_BUFFER.msg_flag:
+            # while False:
+            #   pass
+            mock_t = t_array
+            mock_d = d_array
 
-      # Update observation vector to send to the next time step
-      new_observation_vector = np.zeros((6,24,32), dtype=np.float32)
-      new_observation_vector[0:4, 0, 0] = [i for i in new_pipeline.d_array]
-      new_observation_vector[4, 0, 0] = new_pipeline.t_percent
-      new_observation_vector[5] = new_pipeline.t_array
-      self._observation = new_observation_vector
-      
-    else:
-      raise ValueError('`action` should be 0 to 3.')
+            # Convert to usable arrays NUMPY arrs
+            # t_array, d_array, lat, long = DATA_BUFFER.buffer_to_arrays()
+            # mock_t = np.array([float(i) for i in range(t_dim[0]*t_dim[1])], dtype=np.float32).reshape(t_dim)
+            # mock_d = np.array([100, 100, 25, 25], dtype=np.float32)
 
-    if self._episode_ended or self._observation[4, 0, 0] > .85:
-      # reward = self._pls[self._pl_idx].get_reward(self._observation_spec)
-      return ts.termination(reset_observation_vector, reward=100)
-    else:
-      # Valid action, carry out next turn
-      print("Transmitting Next Turn to RC car...")
-      # TODO: Uncomment
-      # publish.single("HeatSeekingCar/tx", f"{mock_turn}", hostname="test.mosquitto.org")
-      # DATA_BUFFER.msg_flag = False
+            # Generate pipeline for next step
+            new_pipeline = Pipeline(mock_t, mock_d, prev_t_percent=self._observation[4], prev_d_score=prev_d_score, width=t_dim[1], height=t_dim[0])
 
-      return ts.transition(
-          np.array(new_observation_vector, dtype=np.float32), reward=new_pipeline.reward, discount=1.0)
+            # TODO - remove
+            new_pipeline.t_percent = self._observation[4, 0, 0]+.2
+
+            self._pl_hist.add_step(new_pipeline)
+
+            # Update observation vector to send to the next time step
+            new_observation_vector = np.zeros((6,t_dim[0],t_dim[1]), dtype=np.float32)
+            new_observation_vector[0:4, 0, 0] = [i for i in new_pipeline.d_array]
+            new_observation_vector[4, 0, 0] = new_pipeline.t_percent
+            new_observation_vector[5] = new_pipeline.t_array
+            self._observation = new_observation_vector
+        
+
+            return ts.transition(
+                np.array(new_observation_vector, dtype=np.float32), reward=new_pipeline.reward, discount=1.0)
 
 if __name__ == "__main__":
-  test_env = RCCarEnv()
-  # utils.validate_py_environment(test_env, episodes=5)
+    test_env = RCCarEnv()
+    utils.validate_py_environment(test_env, episodes=5)
 
-  tf_env = tf_py_environment.TFPyEnvironment(test_env)
-  # reset() creates the initial time_step after resetting the environment.
-  time_step = tf_env.reset()
-  num_steps = 4
-  transitions = []
-  reward = 0
-  for i in range(num_steps):
-    action = tf.constant(i)
-    # applies the action and returns the new TimeStep.
-    next_time_step = tf_env.step(action)
-    transitions.append([time_step, action, next_time_step])
-    reward += next_time_step.reward
-    time_step = next_time_step
-    print(time_step)
+    tf_env = tf_py_environment.TFPyEnvironment(test_env)
+    # reset() creates the initial time_step after resetting the environment.
+    time_step = tf_env.reset()
+    num_steps = 4
+    transitions = []
+    reward = 0
+    for i in range(num_steps):
+        action = tf.constant(i)
+        # applies the action and returns the new TimeStep.
+        next_time_step = tf_env.step(action)
+        transitions.append([time_step, action, next_time_step])
+        reward += next_time_step.reward
+        time_step = next_time_step
+        print(time_step)
 
-  np_transitions = tf.nest.map_structure(lambda x: x.numpy(), transitions)
-  print('\n'.join(map(str, np_transitions)))
-  print('Total reward:', reward.numpy())
-  # get_new_action = np.array([0, 1, 2, 3], dtype=np.int32)
-  # end_round_action = np.array(4, dtype=np.int32)
+    np_transitions = tf.nest.map_structure(lambda x: x.numpy(), transitions)
+    print('\n'.join(map(str, np_transitions)))
+    print('Total reward:', reward.numpy())
+    # get_new_action = np.array([0, 1, 2, 3], dtype=np.int32)
+    # end_round_action = np.array(4, dtype=np.int32)
 
-  # new_obs_vectors = np.array( [[30, 100, 30, 30, .65],
-  #                             [30, 100, 30, 30, .65],
-  #                             [30, 100, 30, 30, .65],
-  #                             [30, 100, 30, 30, .65],])
+    # new_obs_vectors = np.array( [[30, 100, 30, 30, .65],
+    #                             [30, 100, 30, 30, .65],
+    #                             [30, 100, 30, 30, .65],
+    #                             [30, 100, 30, 30, .65],])
 
-  # environment = RCCarEnv()
-  # time_step = environment.reset()
-  # print(time_step)
-  # cumulative_reward = time_step.reward
+    # environment = RCCarEnv()
+    # time_step = environment.reset()
+    # print(time_step)
+    # cumulative_reward = time_step.reward
 
-  # # for i in range(3):
-  # #   time_step = environment.step(get_new_card_action)
-  # #   print(time_step)
-  # #   cumulative_reward += time_step.reward
+    # # for i in range(3):
+    # #   time_step = environment.step(get_new_card_action)
+    # #   print(time_step)
+    # #   cumulative_reward += time_step.reward
 
-  # time_step = environment.step(end_round_action)
-  # print(time_step)
-  # cumulative_reward += time_step.reward
-  # print('Final Reward = ', cumulative_reward)
+    # time_step = environment.step(end_round_action)
+    # print(time_step)
+    # cumulative_reward += time_step.reward
+    # print('Final Reward = ', cumulative_reward)
 
 
 
-########
+    ########
