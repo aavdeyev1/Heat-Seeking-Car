13d4a592afea79d6e3c23ba56af67ef5781ae36c
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2022-05-11 01:34:46 -0700

    add checkfortimout and use checkiffatal

--- environment.py
+++ environment.py
@@ -1,13 +1,4 @@
 """The environment setup to run the heat-seeking car in.
-observation: This is the part of the environment state that the agent can observe to choose its actions at the next step.
-reward: The agent is learning to maximize the sum of these rewards across multiple steps.
-step_type: Interactions with the environment are usually part of a sequence/episode. e.g. multiple moves in a game of chess. step_type can be either FIRST, MID or LAST to indicate whether this time step is the first, intermediate or last step in a sequence.
-discount: This is a float representing how much to weight the reward at the next time step relative to the reward at the current time step.
-These are grouped into a named tuple TimeStep(step_type, reward, discount, observation).
-
-The interface that all Python environments must implement is in environments/py_environment.PyEnvironment. The main methods are:
-
-
 """
 from __future__ import absolute_import
 from __future__ import division
@@ -31,19 +22,13 @@ from tf_agents.environments import wrappers
 from tf_agents.environments import suite_gym
 from tf_agents.trajectories import time_step as ts
 
-TIMOUT_MINS = 5
+TIMOUT_MINS = 15
 # t_dim = (24, 32)
 t_dim = (12, 16)
 
-# reset_t_array = np.zeros((24,32), dtype=np.float32)
-reset_observation_vector = np.zeros((6,t_dim[0],t_dim[1]), dtype=np.float32)
+reset_observation_vector = np.zeros((6,t_dim[0],t_dim[1]), dtype=np.int32)
 reset_observation_vector[:, 0, 0] = [500.0, 500.0, 500.0, 500.0, 0.1, 0]
 
-def mock_buffer_to_arrays(old_d, old_t):
-    new_d = old_d
-    new_t = old_t
-    new_t = new_t*new_t
-    return new_d, new_t
 
 class RCCarEnv(py_environment.PyEnvironment):
 
@@ -53,7 +38,7 @@ class RCCarEnv(py_environment.PyEnvironment):
         
         # [N, E, S, W, %] distances, current % of thermal view
         self._observation_spec = array_spec.BoundedArraySpec(
-            shape=(6,t_dim[0],t_dim[1]), dtype=np.float32, minimum=0, maximum=500.0, name='observation')
+            shape=(6,t_dim[0],t_dim[1]), dtype=np.int32, minimum=0, maximum=500, name='observation')
 
         self._observation = reset_observation_vector
 
@@ -67,6 +52,13 @@ class RCCarEnv(py_environment.PyEnvironment):
         self._observation_space_size = t_dim[0]*t_dim[1]*(self._action_spec.maximum + 1)
 
         self.conn = Connector()
+    
+    def check_timeout(self):
+        if time.time() > self._timout_after:
+            self.conn.loop_stop()
+            return True
+        else:
+            return False
 
     def action_spec(self):
         return self._action_spec
@@ -82,7 +74,7 @@ class RCCarEnv(py_environment.PyEnvironment):
 
     def _step(self, next_action):
         # TODO: uncomment  when figure out attribute not found error..
-        # self.conn.check_timeout()
+        self.check_timeout()
         lat = None
 
         if self._episode_ended:
@@ -104,51 +96,46 @@ class RCCarEnv(py_environment.PyEnvironment):
             # Calculate prev/current reward
             prev_pl = self._pl_hist.current_pl
             prev_d_score = prev_pl.d_score
-            # prev_reward = prev_pl.get_reward(prev_t_percent=prev_t, prev_d_score=prev_d)
 
-            # busy wait til next Buffer comes in
+            # # busy wait til next Buffer comes in
             while lat == None:
                 t_array, d_array, lat, long = self.conn.check_for_data()
-                time.sleepsleep(1)
-            
+                time.sleep(1)    
         else:
             raise ValueError('`action` should be 0 to 3.')
 
         if self._episode_ended or self._observation[4, 0, 0] > .85:
-            # reward = self._pls[self._pl_idx].get_reward(self._observation_spec)
             return ts.termination(reset_observation_vector, reward=100)
         else:
-            # Valid action, carry out next turn
+            # Check if fatal action
+            self._pl_hist.current_pl.check_if_fatal(next_action)
+            # Valid action returned guaranteed, carry out next turn
             self.conn.send_turn(next_action)
-            # TODO: remove + replace with while not DATA_BUFFER.msg_flag:
-            # while False:
-            #   pass
             mock_t = t_array
             mock_d = d_array
 
+            # TODO - remove;
             # Convert to usable arrays NUMPY arrs
-            # t_array, d_array, lat, long = DATA_BUFFER.buffer_to_arrays()
-            # mock_t = np.array([float(i) for i in range(t_dim[0]*t_dim[1])], dtype=np.float32).reshape(t_dim)
-            # mock_d = np.array([100, 100, 25, 25], dtype=np.float32)
+            # mock_t = np.array([float(i) for i in range(t_dim[0]*t_dim[1])], dtype=np.int32).reshape(t_dim)
+            # mock_d = np.array([100, 100, 25, 25], dtype=np.int32)
 
             # Generate pipeline for next step
-            new_pipeline = Pipeline(mock_t, mock_d, prev_t_percent=self._observation[4], prev_d_score=prev_d_score, width=t_dim[1], height=t_dim[0])
+            new_pipeline = Pipeline(mock_t, mock_d, prev_t_percent=self._observation[4, 0, 0], prev_d_score=prev_d_score, width=t_dim[1], height=t_dim[0])
 
             # TODO - remove
-            new_pipeline.t_percent = self._observation[4, 0, 0]+.2
+            # new_pipeline.t_percent = self._observation[4, 0, 0]+.2
 
             self._pl_hist.add_step(new_pipeline)
 
             # Update observation vector to send to the next time step
-            new_observation_vector = np.zeros((6,t_dim[0],t_dim[1]), dtype=np.float32)
+            new_observation_vector = np.zeros((6,t_dim[0],t_dim[1]), dtype=np.int32)
             new_observation_vector[0:4, 0, 0] = [i for i in new_pipeline.d_array]
             new_observation_vector[4, 0, 0] = new_pipeline.t_percent
             new_observation_vector[5] = new_pipeline.t_array
             self._observation = new_observation_vector
-        
 
             return ts.transition(
-                np.array(new_observation_vector, dtype=np.float32), reward=new_pipeline.reward, discount=1.0)
+                np.array(new_observation_vector, dtype=np.int32), reward=new_pipeline.reward, discount=1.0)
 
 if __name__ == "__main__":
     test_env = RCCarEnv()
@@ -172,29 +159,3 @@ if __name__ == "__main__":
     np_transitions = tf.nest.map_structure(lambda x: x.numpy(), transitions)
     print('\n'.join(map(str, np_transitions)))
     print('Total reward:', reward.numpy())
-    # get_new_action = np.array([0, 1, 2, 3], dtype=np.int32)
-    # end_round_action = np.array(4, dtype=np.int32)
-
-    # new_obs_vectors = np.array( [[30, 100, 30, 30, .65],
-    #                             [30, 100, 30, 30, .65],
-    #                             [30, 100, 30, 30, .65],
-    #                             [30, 100, 30, 30, .65],])
-
-    # environment = RCCarEnv()
-    # time_step = environment.reset()
-    # print(time_step)
-    # cumulative_reward = time_step.reward
-
-    # # for i in range(3):
-    # #   time_step = environment.step(get_new_card_action)
-    # #   print(time_step)
-    # #   cumulative_reward += time_step.reward
-
-    # time_step = environment.step(end_round_action)
-    # print(time_step)
-    # cumulative_reward += time_step.reward
-    # print('Final Reward = ', cumulative_reward)
-
-
-
-    ########
