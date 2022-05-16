bfb2531a255b04eaca2da97504c5cd2f5572651b
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2022-05-16 13:04:42 -0700

    save episode number feature

--- q_learning_script.py
+++ q_learning_script.py
@@ -7,18 +7,19 @@ from math import ceil
 from numpy import savetxt, loadtxt
 from errors import SaveAndExitError
 
-def save_and_exit(f, e):
+def save_and_exit(qtable_file, eps_file, qtable_save, eps_save):
     # Save qtable so we don't error out and lose progress
-    savetxt(f, qtable, delimiter=',')
-    e.write(f'{epsilon}\n')
-    f.close()
-    e.close()
+    savetxt(qtable_file, qtable_save, delimiter=',', encoding="utf-8")
+    eps_file.write(f'{eps_save}\n')
+    qtable_file.close()
+    eps_file.close()
     sys.exit("Saved and Exited...")
 
 # Saved Qtable Params and epsilon
-initialized = True
-q_filename = "qtable_5_16.csv"
-eps_filename = "epsilon_5_16.txt"
+initialized = False
+q_filename = "q_table_5_16.csv"
+eps_filename = "q_epsilon_5_16.txt"
+epi_filename = "q_episode_5_16.txt"
 
 # Init environment
 env = environment.RCCarEnv()
@@ -30,13 +31,13 @@ values_min = 25
 # Distances are 1-400 rounded to the nearest tenth in 4 directions
 max_distance = int(ceil((400 - 10) / 10))
 num_dirs = 4
-action_size = env._action_spec.maximum + 1
+action_size = env.action_spec + 1
 print("Action size ", action_size)
-state_size = env._observation_space_size*(values_max-values_min)*max_distance*num_dirs
+state_size = env.observation_space_size*(values_max-values_min)*max_distance*num_dirs
 print("State size ", state_size)
 
 # Hyperparams
-total_episodes = 0        # Total episodes 50000
+total_episodes = 100        # Total episodes 50000
 total_test_episodes = 1     # Total test episodes 100
 max_steps = 99                # Max steps per episode 99
 
@@ -51,8 +52,8 @@ decay_rate = 0.01             # Exponential decay rate for exploration prob
 
 if initialized:
     # Load previous qtable or initialize
-    f = open(q_filename, "r")
-    e = open(eps_filename, "r")
+    f = open(q_filename, "r", encoding="utf-8")
+    e = open(eps_filename, "r", encoding="utf-8")
     qtable = loadtxt(f, delimiter=',')
     for line in e.readlines():
         epsilon = float(line)
@@ -62,8 +63,8 @@ else:
     qtable = np.zeros((state_size, action_size))
     epsilon = 1.0
 
-f = open(q_filename, "w")
-e = open(eps_filename, "a")
+f = open(q_filename, "w", encoding="utf-8")
+e = open(eps_filename, "a", encoding="utf-8")
 try:
     # Take the action (a) and observe the outcome state(s') and reward (r)
     # 2 For life or until learning is stopped
@@ -76,7 +77,7 @@ try:
         # Save Qtable just in case...
         savetxt(f, qtable, delimiter=',')
         e.write(f'{epsilon}\n')
-        
+
         for step in range(max_steps):
             # 3. Choose an action a in the current world state (s)
             ## First we randomize a number
@@ -95,16 +96,16 @@ try:
                 # Take the action (a) and observe the outcome state(s') and reward (r)
                 new_obs, reward, done, info = env.step(action)
             except SaveAndExitError:
-                save_and_exit(f, e)
+                save_and_exit(f, e, qtable, epsilon)
             except KeyboardInterrupt:
-                save_and_exit(f, e)
+                save_and_exit(f, e, qtable, epsilon)
 
             # Update Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
             try:
                 qtable[obs, action] = qtable[obs, action] + learning_rate * (reward + gamma *
                                             np.max(qtable[info, :]) - qtable[obs, action])
             except IndexError:
-                save_and_exit(f, e)
+                save_and_exit(f, e, qtable, epsilon)
 
             # Our new state is state
             obs = info
@@ -114,23 +115,22 @@ try:
                 break
 
         # Reduce epsilon (because we need less and less exploration)
-        epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode) 
+        epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode)
+
+        # Save the num of episode for future reference
+        with open(epi_filename, "a", encoding="utf-8") as i:
+            for line in i.readlines():
+                prev_ep_num = int(line)
+                prev_ep_num += 1
+                i.write(f'{prev_ep_num}\n')
 
 except SaveAndExitError:
-    save_and_exit(f, e)
+    save_and_exit(f, e, qtable, epsilon)
 except KeyboardInterrupt:
-    save_and_exit(f, e)
+    save_and_exit(f, e, qtable, epsilon)
 
-save_and_exit(f, e)
+save_and_exit(f, e, qtable, epsilon)
 
-# # save qtable to csv file
-# savetxt(f, qtable, delimiter=',')
-
-# # save epsilon
-# e.write(f'{epsilon}\n')
-
-# f.close()
-# e.close()
 # # For Testing The Qtable
 # env.reset()
 # rewards = []
@@ -148,11 +148,11 @@ save_and_exit(f, e)
 #         # env.render()
 #         # Take the action (index) that have the maximum expected future reward given that state
 #         action = np.argmax(qtable[state,:])
-        
+
 #         new_state, reward, done, info = env.step(action)
-        
+
 #         total_rewards += reward
-        
+
 #         if done:
 #             rewards.append(total_rewards)
 #             #print ("Score", total_rewards)
