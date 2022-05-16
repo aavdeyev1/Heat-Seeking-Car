6a29ce8172057e0ad35080f781f75ab9ce206b02
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2022-05-16 10:16:43 -0700

    add save_and_exit method save, update and use qtable, eps. Add error clauses.

--- q_learning_script.py
+++ q_learning_script.py
@@ -7,9 +7,18 @@ from math import ceil
 from numpy import savetxt, loadtxt
 from errors import SaveAndExitError
 
-# Saved Qtable Params
-initialized = False
-filename = "qtable_5_11.csv"
+def save_and_exit(f, e):
+    # Save qtable so we don't error out and lose progress
+    savetxt(f, qtable, delimiter=',')
+    e.write(f'{epsilon}\n')
+    f.close()
+    e.close()
+    sys.exit("Saved and Exited...")
+
+# Saved Qtable Params and epsilon
+initialized = True
+q_filename = "qtable_5_16.csv"
+eps_filename = "epsilon_5_16.txt"
 
 # Init environment
 env = environment.RCCarEnv()
@@ -35,63 +44,93 @@ learning_rate = 0.7           # Learning rate
 gamma = 0.618                 # Discounting rate
 
 # Exploration parameters
-epsilon = 1.0                 # Exploration rate
+# epsilon = 1.0                 # Exploration rate
 max_epsilon = 1.0             # Exploration probability at start
 min_epsilon = 0.01            # Minimum exploration probability 
 decay_rate = 0.01             # Exponential decay rate for exploration prob
 
-# Load previous qtable or initialize
-f = open(filename, "w")
 if initialized:
+    # Load previous qtable or initialize
+    f = open(q_filename, "r")
+    e = open(eps_filename, "r")
     qtable = loadtxt(f, delimiter=',')
+    for line in e.readlines():
+        epsilon = float(line)
+    f.close()
+    e.close()
 else:
     qtable = np.zeros((state_size, action_size))
+    epsilon = 1.0
 
-# 2 For life or until learning is stopped
-for episode in range(total_episodes):
-    # Reset the environment
-    obs = env.reset().observation
-    step = 0
-    done = False
-    
-    for step in range(max_steps):
-        # 3. Choose an action a in the current world state (s)
-        ## First we randomize a number
-        exp_exp_tradeoff = random.uniform(0,1)
-        
-        ## If this number > greater than epsilon --> exploitation (taking the biggest Q value for this state)
-        if exp_exp_tradeoff > epsilon:
-            action = np.argmax(qtable[obs,:])
-        
-        # Else doing a random choice --> exploration
-        else:
-            action = random.randrange(4)
-        
-        try:
-            # Take the action (a) and observe the outcome state(s') and reward (r)
-            new_obs, reward, done, info = env.step(action)
-        except SaveAndExitError:
-            # Save qtable so we don't error out and lose progress
-            savetxt(f, qtable, delimiter=',')
-            sys.exit("Saved and Exited...")
+f = open(q_filename, "w")
+e = open(eps_filename, "a")
+try:
+    # Take the action (a) and observe the outcome state(s') and reward (r)
+    # 2 For life or until learning is stopped
+    for episode in range(total_episodes):
+        # Reset the environment
+        obs = env.reset().observation
+        step = 0
+        done = False
 
-        # Update Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
-        qtable[obs, action] = qtable[obs, action] + learning_rate * (reward + gamma * 
-                                    np.max(qtable[info, :]) - qtable[obs, action])
-                
-        # Our new state is state
-        obs = info
+        # Save Qtable just in case...
+        savetxt(f, qtable, delimiter=',')
+        e.write(f'{epsilon}\n')
         
-        # If done : finish episode
-        if done == True: 
-            break
-    
-    # Reduce epsilon (because we need less and less exploration)
-    epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode) 
+        for step in range(max_steps):
+            # 3. Choose an action a in the current world state (s)
+            ## First we randomize a number
+            exp_exp_tradeoff = random.uniform(0,1)
 
-# save qtable to csv file
-savetxt(f, qtable, delimiter=',')
+            ## If this number > greater than epsilon --> exploitation
+            # (taking the biggest Q value for this state)
+            if exp_exp_tradeoff > epsilon:
+                action = np.argmax(qtable[obs,:])
 
+            # Else doing a random choice --> exploration
+            else:
+                action = random.randrange(4)
+
+            try:
+                # Take the action (a) and observe the outcome state(s') and reward (r)
+                new_obs, reward, done, info = env.step(action)
+            except SaveAndExitError:
+                save_and_exit(f, e)
+            except KeyboardInterrupt:
+                save_and_exit(f, e)
+
+            # Update Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
+            try:
+                qtable[obs, action] = qtable[obs, action] + learning_rate * (reward + gamma *
+                                            np.max(qtable[info, :]) - qtable[obs, action])
+            except IndexError:
+                save_and_exit(f, e)
+
+            # Our new state is state
+            obs = info
+
+            # If done : finish episode
+            if reward == 100:
+                break
+
+        # Reduce epsilon (because we need less and less exploration)
+        epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode) 
+
+except SaveAndExitError:
+    save_and_exit(f, e)
+except KeyboardInterrupt:
+    save_and_exit(f, e)
+
+save_and_exit(f, e)
+
+# # save qtable to csv file
+# savetxt(f, qtable, delimiter=',')
+
+# # save epsilon
+# e.write(f'{epsilon}\n')
+
+# f.close()
+# e.close()
 # # For Testing The Qtable
 # env.reset()
 # rewards = []
